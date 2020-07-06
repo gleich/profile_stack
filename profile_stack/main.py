dc5b37f70f44647be8b1120bc9e8e1f596ed8e56
Matthew Gleich <matthewgleich@gmail.com>
2020-07-05 21:04:15 -0400

    ♻️ Refactor table generator to use list of lines
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- profile_stack/main.py
+++ profile_stack/main.py
@@ -1,4 +1,5 @@
 import config
+import table
 from loguru import logger
 
 
@@ -6,14 +7,14 @@ def main():
     """Main function for the program
     """
     config_contents = config.load_config('/github/workspace')
-    table = table.generate_table(config_contents)
+    table_lines = table.generate_table(config_contents)
     with open('README.md') as readme_file:
         readme_orig_lines = readme_file.readlines()
     if '<!-- START OF PROFILE STACK, DO NOT REMOVE -->' not in readme_orig_lines:
         with open('README.md', 'a') as readme_file:
             readme_file.write(
                 '\n<!-- START OF PROFILE STACK, DO NOT REMOVE -->')
-            readme_file.write(table)
+            readme_file.writelines(table_lines)
             readme_file.write('<!-- END OF PROFILE STACK, DO NOT REMOVE -->')
             logger.success('Wrote table to README.md file')
     else:
@@ -27,7 +28,7 @@ def main():
                 if write_line:
                     readme_file.write(line)
                 if line == '<!-- START OF PROFILE STACK, DO NOT REMOVE -->':
-                    readme_file.write(table)
+                    readme_file.writelines(table_lines)
                     write_line = False
                 logger.success('Wrote table to README.md file')
 
--- profile_stack/table.py
+++ profile_stack/table.py
@@ -10,7 +10,7 @@ def generate_table(config):
         config (dictionary): The config file
 
     Returns:
-        str: The table
+        list: All the lines for the table
     """
     technology_emoji = os.getenv('INPUT_TECHNOLOGY_EMOJI')
     projects_emoji = os.getenv('INPUT_PROJECT_EMOJI')
@@ -26,4 +26,4 @@ def generate_table(config):
         combined_project_badges = ' '.join(project_badges)
         rows.append(f'| {technology_badge} | {combined_project_badges} |')
     logger.success('Generated table')
-    return '\n'.join(rows)
+    return rows
