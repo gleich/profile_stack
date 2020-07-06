62eb12b1420e211384ea8b94d59efd0edd1e2c4a
Matthew Gleich <matthewgleich@gmail.com>
2020-07-05 22:44:18 -0400

    🐛 Tell git who is committing
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- profile_stack/main.py
+++ profile_stack/main.py
@@ -14,9 +14,10 @@ def main():
     if '<!-- START OF PROFILE STACK, DO NOT REMOVE -->' not in readme_orig_lines:
         with open('README.md', 'a') as readme_file:
             readme_file.write(
-                '\n<!-- START OF PROFILE STACK, DO NOT REMOVE -->')
-            readme_file.writelines(table_lines)
-            readme_file.write('<!-- END OF PROFILE STACK, DO NOT REMOVE -->')
+                '\n<!-- START OF PROFILE STACK, DO NOT REMOVE -->\n')
+            for table_line in table_lines:
+                readme_file.write(table_line + '\n')
+            readme_file.write('<!-- END OF PROFILE STACK, DO NOT REMOVE -->\n')
             logger.success('Wrote table to README.md file')
     else:
         with open('README.md', 'w') as readme_file:
@@ -24,14 +25,17 @@ def main():
         with open('README.md', 'a') as readme_file:
             write_line = True
             for line in readme_orig_lines:
-                if line == '<!-- END OF PROFILE STACK, DO NOT REMOVE -->':
+                if line == '\n<!-- END OF PROFILE STACK, DO NOT REMOVE -->\n':
                     write_line = True
                 if write_line:
-                    readme_file.write(line)
-                if line == '<!-- START OF PROFILE STACK, DO NOT REMOVE -->':
-                    readme_file.writelines(table_lines)
+                    readme_file.write(line + '\n')
+                if line == '<!-- START OF PROFILE STACK, DO NOT REMOVE -->\n':
+                    for table_line in table_lines:
+                        readme_file.write(table_line + '\n')
                     write_line = False
                 logger.success('Wrote table to README.md file')
+    os.system('git config --global user.email "action@github.com"')
+    os.system('git config --global user.name "Publishing Bot"')
     os.system('git add .')
     os.system('git commit -m "Update profile stack"')
     os.system('git push')
