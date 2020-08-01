61813eef424b78870ba1c564f0982803c43d63d2
Matthew Gleich <matthewgleich@gmail.com>
2020-08-01 00:51:55 -0400

    🔇 Remove command logs
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- profile_stack/commands.py
+++ profile_stack/commands.py
@@ -7,12 +7,13 @@ def run_commands(commands, error_message):
 
     Args:
         commands (list of strings): Commands to run
-        error_message ([type]): [description]
+        error_message (string): Error message to be displayed to the user
     """
     for command in commands:
         exit_code = os.system(command)
-        print(f'COMMAND   = {command}')
-        print(f'EXIT_CODE = {exit_code}')
-        if exit_code != 0:
+        if exit_code == 256:
+            print('Stopped early')
+            exit(0)
+        elif exit_code != 0:
             logger.error(error_message)
             exit(1)
