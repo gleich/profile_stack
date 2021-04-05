986678e3da6de238eb1f7b36b1d5b42441d6ace5
Matthew Gleich <git@mattglei.ch>
2021-04-05 00:55:38 -0400

    test: add test for Env::get
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/conf.rs
+++ src/conf.rs
@@ -1,10 +1,11 @@
 use std::env;
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
 
 #[derive(Debug)]
 pub struct Env {
-	pub path: String,
+	pub path: PathBuf,
 	pub badges: bool,
 	pub technology_emoji: char,
 	pub project_emoji: char,
@@ -18,10 +19,25 @@ impl Env {
 
 	pub fn get() -> Result<Env, anyhow::Error> {
 		Ok(Env {
-			path: Env::get_env_var("path", "stack.yml")?,
+			path: Path::new(Env::get_env_var("path", "stack.yml")?.as_str()).to_owned(),
 			badges: Env::get_env_var("badges", "true")?.parse()?,
 			technology_emoji: Env::get_env_var("technology_emoji", "💻")?.parse()?,
 			project_emoji: Env::get_env_var("project_emoji", "🚀")?.parse()?,
 		})
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_env_get() -> Result<(), anyhow::Error> {
+		let env_var_conf = Env::get()?;
+		assert_eq!(env_var_conf.path, Path::new("stack.yml"));
+		assert_eq!(env_var_conf.badges, true);
+		assert_eq!(env_var_conf.technology_emoji, '💻');
+		assert_eq!(env_var_conf.project_emoji, '🚀');
+		Ok(())
 	}
 }
